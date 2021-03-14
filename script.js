e87d03efa4aca375b16aa2a32daba301ed9dc50c
Omar <omarsaleh92@gmail.com>
2021-03-13 21:35:14 -0600

    Add JS logic to determine the winner

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const oBtn = document.getElementById('o-button');
 const xBtn = document.getElementById('x-button');
 const rBtn = document.getElementById('r-button');
 
+const playerFactory = (mark) => {
+  const playerMarks = ['', '', '', '', '', '', '', '', ''];
+  return { playerMarks };
+};
+const oPlayer = playerFactory('O');
+const xPlayer = playerFactory('X');
+
 const board = (() => {
   /* -------------------------------- variables ------------------------------- */
 
@@ -12,9 +19,9 @@ const board = (() => {
   /* --------------------------------- methods -------------------------------- */
 
   const addEvents = () => {
-    oBtn.addEventListener('click', board.getMark);
-    xBtn.addEventListener('click', board.getMark);
-    rBtn.addEventListener('click', board.resetBoard);
+    // oBtn.addEventListener('click', board.getMark);
+    // xBtn.addEventListener('click', board.getMark);
+    // rBtn.addEventListener('click', board.resetBoard);
   };
   const renderBoard = () => {
     let idCounter = 0;
@@ -34,10 +41,11 @@ const board = (() => {
       }
     });
   };
-
   const resetBoard = () => {
     currentMark = '';
     boardMarks = ['', '', '', '', '', '', '', '', ''];
+    oPlayer.playerMarks = ['', '', '', '', '', '', '', '', ''];
+    xPlayer.playerMarks = ['', '', '', '', '', '', '', '', ''];
     gameboard.innerHTML = '';
     renderBoard();
   };
@@ -53,6 +61,16 @@ const board = (() => {
       DOMCell.target.innerText = currentMark;
       const i = DOMCell.target.id;
       boardMarks[i] = currentMark;
+
+      if (currentMark === 'O') {
+        oPlayer.playerMarks[i] = currentMark;
+        console.log(oPlayer.playerMarks);
+        winCheck.checkWinner(oPlayer.playerMarks, currentMark);
+      } else {
+        xPlayer.playerMarks[i] = currentMark;
+        console.log(xPlayer.playerMarks);
+        winCheck.checkWinner(xPlayer.playerMarks, currentMark);
+      }
       //   console.log(boardMarks);
     }
   };
@@ -74,5 +92,49 @@ const board = (() => {
   };
 })();
 
+const winCheck = (() => {
+  const xWinningCombo = [
+    ['X', 'X', 'X', '', '', '', '', '', ''],
+    ['', '', '', 'X', 'X', 'X', '', '', ''],
+    ['', '', '', '', '', '', 'X', 'X', 'X'],
+    ['X', '', '', '', 'X', '', '', '', 'X'],
+    ['', 'X', '', '', 'X', '', '', 'X', ''],
+    ['', '', 'X', '', 'X', '', 'X', '', ''],
+  ];
+  const oWinningCombo = [
+    ['O', 'O', 'O', '', '', '', '', '', ''],
+    ['', '', '', 'O', 'O', 'O', '', '', ''],
+    ['', '', '', '', '', '', 'O', 'O', 'O'],
+    ['O', '', '', '', 'O', '', '', '', 'O'],
+    ['', 'O', '', '', 'O', '', '', 'O', ''],
+    ['', '', 'O', '', 'O', '', 'O', '', ''],
+  ];
+  const checkWinner = (array, mark) => {
+    switch (mark) {
+      case 'X':
+        if (
+          xWinningCombo.some((a) => array.every((v, i) => v === a[i]))
+        ) {
+          console.log('X Wins!');
+        }
+        break;
+      case 'O':
+        if (
+          oWinningCombo.some((a) => array.every((v, i) => v === a[i]))
+        ) {
+          console.log('O Wins!');
+        }
+        break;
+      default:
+        console.log('nothing');
+    }
+  };
+  return { checkWinner };
+})();
+
 board.renderBoard();
 board.addEvents();
+
+oBtn.addEventListener('click', board.getMark);
+xBtn.addEventListener('click', board.getMark);
+rBtn.addEventListener('click', board.resetBoard);
